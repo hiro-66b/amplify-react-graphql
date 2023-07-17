039c8ea0d608ab083e715aa2126796d3915c7ce4
hiro-66b <m.hiro66b@gmail.com>
2023-07-17 15:36:10 +0900

    [init]

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-Expand All
-	@@ -6,20 +7,10 @@ function App() {
+function App() {
+  return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
